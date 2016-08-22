bc7e86cc8dd11a0cadf1fd0e8f7897d06d76aac5
Andrej Saibel <andrej.saibel@cern.ch>
2016-08-22 10:47:14 +0200

    crabconf

--- crabconfig.py
+++ crabconfig.py
@@ -8,15 +8,16 @@ config.General.transferLogs = True
 
 config.section_("JobType")
 config.JobType.pluginName = 'PrivateMC'
-config.JobType.psetName = 'pythonLHEGEN_cfg.py'
+config.JobType.psetName = 'pythonLHEGENFilter2_cfg.py'
 config.JobType.inputFiles = ["run_generic_tarball_cvmfs_modified.sh"]
 config.JobType.disableAutomaticOutputCollection = False
+config.JobType.outputFiles = ["eventLHEGEN-output_inLHE.root","eventLHEGEN-output.root"]
 
 config.section_("Data")
 config.Data.outputPrimaryDataset = 'privateMCProductionLHEGEN'
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 100
-config.Data.totalUnits = 1000
+config.Data.unitsPerJob = 5
+config.Data.totalUnits = 5
 config.Data.publication = False
 config.Data.outputDatasetTag = 'eventLHEGEN'
 
@@ -25,4 +26,3 @@ config.Site.storageSite = 'T2_DE_DESY'
 
 config.section_("User")
 config.User.voGroup = "dcms"
-
